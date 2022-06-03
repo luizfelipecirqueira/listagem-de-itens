ce4d9892004badbcf844751dacf41dfdd7e46935
luizfelipecirqueira <lfcs18ts@gmail.com>
2022-06-03 00:34:39 -0300

    Meu commit do projeto de lista de livros para comprar

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import ListItem from "./components/ListItem";
+import { getBooks } from './components/BooksService';
+import { Container } from 'react-bootstrap';
+import styled from 'styled-components';
+
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: #ccc;
+  padding: 10px;
+`;
+
+function App() {
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    setBooks(getBooks());
+  }, [])
+
+  return (
+    <Container>
+      <ListContainer>
+        {books.map(book => {
+          return (
+            <ListItem
+              key={book.url}
+              title={book.title}
+              image={book.image}
+              price={book.price}
+              url={book.url}
+            />
+          )
+        })}
+      </ListContainer>
+    </Container>
+  );
+}
+
+
+export default App;
